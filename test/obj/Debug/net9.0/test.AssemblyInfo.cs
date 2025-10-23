113bb675b034ba78c8b243ae9b86cc64917638ea
Oluwatadel <oluwatobiadelesi@gmail.com>
2025-10-23 15:30:53 +0100

    git

--- test/obj/Debug/net9.0/test.AssemblyInfo.cs
+++ test/obj/Debug/net9.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d79300cc8b49f37a422b4fda9a4f2b0eee11b4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c248367e1c0b3e291f7c42291b4379b8c361aba")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
